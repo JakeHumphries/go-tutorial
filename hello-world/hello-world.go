295778f95180ef5d3ae35b8196530a6ff597adfc
NandosJake <39186454+NandosJake@users.noreply.github.com>
2021-05-06 08:55:12 +0100

    hello world & short declaration

--- hello-world/hello-world.go
+++ hello-world/hello-world.go
@@ -1,5 +1,24 @@
-package hello
+package main
 
-func hello()  {
-	
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+	foo()
+	fmt.Println("something more")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
 }
+
+func foo() {
+	fmt.Println(("Im in foo"))
+}
+
+// control flow
+// (1) sequence
+// (2) loop; iterative
+// (3) conditional
